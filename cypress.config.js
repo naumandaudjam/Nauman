61d3140a87b29249b982c15a8d83514fb4019446
naumandaudjam <74778430+naumandaudjam@users.noreply.github.com>
2025-11-19 03:22:39 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,11 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  reporter: "allure",
-  reporterOptions: {
-    allureLogCypress: true,
-    allureOmitPreviousAttemptScreenshots: true,
-  },
   e2e: {
     baseUrl: "https://bac-test.xpresspago.com",
     experimentalSessionAndOrigin: true,
     pageLoadTimeout: 120000,
-    env: {
-      allure: true,
-      allureResultsPath: "allure-results",
-      allureAttachRequests: true,
-      allureReuseAfterSpec: true,
-    },
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
       return config;
     },
   },
