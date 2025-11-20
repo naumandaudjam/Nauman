d3bb501b1b831ffcb70c2680ab974609a679dbe0
Nauman Daud Jam <naumandaudjam1996@gmail.com>
2025-11-20 19:39:26 +0500

    code refactor

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "allure",
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    allureLogCypress: true,
-    allureOmitPreviousAttemptScreenshots: true,
+    reportDir: "reports/mocha",
+    reportFilename: "[status]-[datetime]-[name]-report",
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    reportPageTitle: "BAC Automation Mocha Report",
   },
+
   e2e: {
     baseUrl: "https://bac-test.xpresspago.com",
-    experimentalSessionAndOrigin: true,
     pageLoadTimeout: 120000,
+
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
+      require("cypress-mochawesome-reporter/plugin")(on);
       return config;
     },
   },
