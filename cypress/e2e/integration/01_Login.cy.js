4c363332aa08cb98f8cb9303a5d1a594b8c29485
Nauman Daud Jam <naumandaudjam1996@gmail.com>
2025-11-20 22:25:31 +0500

    Code Refactor

--- cypress/e2e/integration/01_Login.cy.js
+++ cypress/e2e/integration/01_Login.cy.js
@@ -1,5 +1,3 @@
-import Login from "../../PageObjects/loginPage";
-
 describe("loginSession", () => {
 
   it("Successfully Login and Verify Dashboard", () => {
--- cypress/support/utilities.js
+++ cypress/support/utilities.js
@@ -1,26 +1,11 @@
-const baseUrl_Utilities = "https://bac-test.xpresspago.com/Home/Index";
 const baseUrl_Client_Utilities = "https://bac-test.xpresspago.com/Client/Index";
-const baseUrl_Bank_Utilities = "https://bac-test.xpresspago.com//Bank/Index";
-const baseUrl_Product_Utilities = "https://bac-test.xpresspago.com//Product/Index";
-const baseUrl_addServices_Utilities = "Cycle/Index";
-const baseUrl_addServices_Utilities_Success = "https://bac-test.xpresspago.com/Product/Index?PageResponse=Saved";
 
 const randomNum_Utilities = String(Math.floor(Math.random() * 1000)).padStart(3, "0");
-const randomPhone_Utilities = `03${Math.floor(100000000 + Math.random() * 900000000)}`;
 const randomEmail_Utilities = `user${randomNum_Utilities}@yopmail.com`;
 const companyName_Utilities = `NaumanAutomationCompany${randomNum_Utilities}`;
-const serviceName_Utilities = `NaumanAutomationServices${randomNum_Utilities}`;
 
 export {
-  baseUrl_Utilities,
-  randomNum_Utilities,
-  randomPhone_Utilities,
+  baseUrl_Client_Utilities,
   randomEmail_Utilities,
   companyName_Utilities,
-  baseUrl_Product_Utilities,
-  baseUrl_Client_Utilities,
-  baseUrl_addServices_Utilities,
-  serviceName_Utilities,
-  baseUrl_addServices_Utilities_Success,
-  baseUrl_Bank_Utilities,
 };
